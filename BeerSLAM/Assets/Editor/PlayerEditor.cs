88f4018c186f29aebad36d86db2a656d33a7b0da
Tylah Kapa <1005732@Student.sae.edu.au>
2017-08-17 23:41:10 +1000

    Main Menu Layout Completed

---
+++ BeerSLAM/Assets/Editor/PlayerEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(Player))]
+[CanEditMultipleObjects]
+public class PlayerEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        Player p = (Player)target;
+
+        EditorGUILayout.LabelField("Input");
+
+        //Start horizontal Line
+        EditorGUILayout.BeginHorizontal();
+
+        //End horizontal line
+        EditorGUILayout.EndHorizontal();
+    }
+}
---
+++ BeerSLAM/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public string throwInput;
+    public string dodgeInput;
+
+    public GameObject beerCan;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+        print("Throw: " + throwInput + " Dodge: " + dodgeInput);
+	}
+}
