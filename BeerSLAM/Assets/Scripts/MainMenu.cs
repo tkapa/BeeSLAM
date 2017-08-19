5fb46a55eb15cf7058ec3815457d9b5dc723b7a4
Tylah Kapa <1005732@Student.sae.edu.au>
2017-08-20 01:14:47 +1000

    Main Menu Script
    Created a main menu script that just polls user input. (AS Player one, KL Player two), and tells the users that each player is ready when both keys are held down. An event manager should be made to tell the game that the game should begin when both players are ready.

---
+++ BeerSLAM/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+    
+    //Button and Text for each Player
+    public Button[] playerOneButtons = new Button[2];
+    public Text playerOneReadyText;
+
+    public Button[] playerTwoButtons = new Button[2];
+    public Text playerTwoReadyText;
+
+    //used to tell whether or not both players are ready
+    private bool isPlayerOneReady = false;
+    private bool isPlayerTwoReady = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TakeInput();
+        StartGame();
+	}
+
+    //Does something with the input for both players
+    void TakeInput()
+    {
+        //If player one is holding down their buttons
+        if (Input.GetKey("a") && Input.GetKey("s"))
+        {
+            playerOneReadyText.text = "Ready!";
+            isPlayerOneReady = true;
+        } else if(Input.GetKeyUp("a") || Input.GetKeyUp("s"))
+        {
+            playerOneReadyText.text = "Not Ready!";
+            isPlayerOneReady = false;
+        }
+
+        //If player two is holding down their buttons
+        if (Input.GetKey("k") && Input.GetKey("l"))
+        {
+            playerTwoReadyText.text = "Ready!";
+            isPlayerTwoReady = true;
+        }
+        else if (Input.GetKeyUp("k") || Input.GetKeyUp("l"))
+        {
+            playerTwoReadyText.text = "Not Ready!";
+            isPlayerTwoReady = false;
+        }
+
+        //Allow for muting of Music (is any)
+        if (Input.GetKey("m"))
+        {
+
+        }
+    }
+
+    //Should the game begin?
+    void StartGame()
+    {
+        //If both players are ready start the game
+        if(isPlayerOneReady && isPlayerTwoReady)
+        {
+            print("Start Game");
+        }
+    }
+}
