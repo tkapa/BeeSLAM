d04f21a670cb0b3a44d09719420cbf0f384e811b
Tylah Kapa <1005732@Student.sae.edu.au>
2017-08-23 23:03:32 +1000

    Setting up new player functionality
    Player can now hold down the throw key to throw harder. The actual throwing implementation still needs to be created. Also changed it so that they throw differently while jumping. Double tapping the dodge key might be finnicky, so we'll focus on just jumping for now.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,6 +19,7 @@ public class Player : MonoBehaviour {
         EPS_Parrying
     }
 
+    [Tooltip("Number for player")]
     public Player_Number playerNumber;
 
     [HideInInspector]
@@ -30,17 +31,28 @@ public class Player : MonoBehaviour {
     //Inputs for this player
     public string throwInput, dodgeInput;
 
+    public float jumpingForce = 1500.0f;
+
     //Am I meant to be able to move
     //(is true for now, make false when testing)
     bool takingInput = true;
+
+    //Manages the player's throwing and when the strength increases 
+    public Vector2 throwThresholds = new Vector2(0.4f, 0.8f);
+    private float throwHoldTime = 0.0f;
+
+    //Manages the player's count to jump or dodge
+    public float doubleTapTime = 0.1f;
+    private float dodgeTime;
 
 
     // Use this for initialization
     void Start () {
         //Check for setup errors
-        if (GetComponent<Rigidbody2D>())
+        if (!GetComponent<Rigidbody2D>())
             Debug.LogError(gameObject.name + " does not contain a rigidbody2D!");
-
+        else rb = GetComponent<Rigidbody2D>();
+        
         if (playerNumber == 0)
             Debug.LogError(gameObject.name + " has not been assigned a player number!");
 
@@ -57,14 +69,38 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //If input is being accepted
-        if (takingInput)
+        //if (takingInput)
             PollInput(Time.deltaTime);
 	}
 
     //Called to take input from the player
     void PollInput(float timeDelta)
     {
+        //While holding down the throw button
+        if (Input.GetKey(throwInput))
+        {
+            throwHoldTime += timeDelta;
 
+            //Ensure to provide feedback to the player
+        }
+        //When the player releases the throw button
+        else if (Input.GetKeyUp(throwInput))
+        {
+            print(throwHoldTime);
+
+            Throw();
+
+            //Reset the hold time
+            throwHoldTime = 0.0f;
+        }
+
+        //When the dodge button is presssed
+        if (Input.GetKeyDown(dodgeInput))
+        {
+            //For now just jump
+            if(playerState == Player_State.EPS_Standing)
+                Jump();
+        }
     }
 
     //Called when the player is hit
@@ -74,5 +110,74 @@ public class Player : MonoBehaviour {
             EventManager.instance.OnEndRound.Invoke(1);
         else
             EventManager.instance.OnEndRound.Invoke(0);
+    }
+
+    void Throw()
+    {
+        //Switch case for throwing
+        switch (playerState)
+        {
+            default:
+                //No throw by default
+                break;
+
+            //Throw for standing
+            case Player_State.EPS_Standing:
+                if (throwHoldTime > throwThresholds.y)
+                {
+
+                }
+                else if (throwHoldTime > throwThresholds.x && throwHoldTime < throwThresholds.y)
+                {
+
+                }
+                else if(throwHoldTime < throwThresholds.x)
+                {
+
+                }
+                break;
+
+            //Throw for jumping
+            case Player_State.EPS_Jumping:
+                if (throwHoldTime > throwThresholds.y)
+                {
+
+                }
+                else if (throwHoldTime > throwThresholds.x && throwHoldTime < throwThresholds.y)
+                {
+
+                }
+                else if (throwHoldTime < throwThresholds.x)
+                {
+
+                }
+                break;
+        }
+    }
+
+    //Called when the player wants to jump
+    void Jump()
+    {
+        //Set to jumping and start to jump
+        playerState = Player_State.EPS_Jumping;
+
+        rb.AddForce(new Vector2(0, jumpingForce));
+    }
+
+    //Coroutine begins wheen the player dodges
+    IEnumerator Dodge()
+    {
+        //Player jumps backwards
+        //Player is set to dodging state while jumping backwards
+        //Player jumps forwasrds again
+        //Player is set to Parry state during this time
+        //Player returns to origin position and is set to standing again
+        yield return null;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground" && playerState == Player_State.EPS_Jumping)
+            playerState = Player_State.EPS_Standing;
     }
 }
